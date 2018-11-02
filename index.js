f4c300c8301d0eff008fe609da9f9a67194b2cd8
maxwondercorn <gmartell@pobox.com>
2018-11-02 15:59:19 -0400

    use cjs-transform

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ module.exports = {
   name: "ember-c3",
   included: function(app) {
     this._super.included(app);
-
+    
     app.import('node_modules/c3/c3.css');
+
+    app.import('node_modules/c3/c3.js', {
+      using: [
+        { transformation: 'amd', as: 'c3'}
+      ]
+    });
+
+    app.import('node_modules/d3/dist/d3.js', {
+      using: [
+        { transformation: 'amd', as: 'd3'}
+      ]
+    });
   
   }
 };
